b3347f6a135e544f1e2c6cb1a8a90354af385573
roamiri <amiriruhollah@gmail.com>
2018-10-29 11:48:58 -0600

    added timing

--- Run_Michael_Nielsen_MNIST_1CP_1FC_Softmax_program.py
+++ Run_Michael_Nielsen_MNIST_1CP_1FC_Softmax_program.py
@@ -8,7 +8,9 @@ Created on Mon Oct 29 11:36:48 2018
 import network3
 from network3 import Network
 from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+import time
 
+start = time.time()
 training_data, validation_data, test_data = network3.load_data_shared()
 mini_batch_size = 10
 net = Network([
@@ -19,3 +21,7 @@ net = Network([
     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
 
 net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data) # λ = 0.
+
+end = time.time()
+
+print('time needed to run program:', end – start)
