0089265a203c13c2a7ecee362a6c64015f2a44ea
roamiri <amiriruhollah@gmail.com>
2018-10-29 11:56:54 -0600

    changed batch file for running on XSEDE

--- HW8_MNIST_1CP_1FC_batch_xsede2.sh
+++ HW8_MNIST_1CP_1FC_batch_xsede2.sh
@@ -15,11 +15,11 @@ set -x
 # copy the code and data to the working directory
 # cp -R $SCRATCH/Michael_A_Nielsen
 # go to the directory where actual runfile is present
-cd $HOME/Michael_A_Nielsen/MichaelNielsen_code_chap6/
+cd $HOME/CNN
 # load all necessary modules
 module load python cuda theano
 # run the python code and copy output file to persistent storage
-python Run_Michael_Nielsen_MNIST_1CP_1FC_Softmax_program.py
+python Run_Michael_Nielsen_MNIST_1CP_1FC_Softmax_program.py >> xsede.txt
 
 
 
